eb12b384e5f9d3dce540f41d64d440fca9a88c49
Joao Lopes <joaolopes71@hotmail.com>
2019-07-18 18:51:27 -0300

    writing and reading records

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,23 +1,58 @@
 package com.pluralsight.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
+import com.pluralsight.repository.util.RideRowMapper;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public List<Ride> getRides() {
-		Ride ride = new Ride();
-		ride.setName("Corner Canyon");
-		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
-		rides.add(ride);
+		
+		List<Ride> rides = jdbcTemplate.query("SELECT * FROM ride", new RideRowMapper());
+		
 		return rides;
+	}
+
+	@Override
+	public Ride createRide(Ride ride) {
+		
+		jdbcTemplate.update("INSERT INTO ride (name, duration) VALUES (?, ?)", ride.getName(), ride.getDuration());
+		
+		/* Outro metodo que existe no SpringJDBC eh o SimpleJdbcInsert, posso fazer essa configuracao
+		 uma vez e usar para todos os metodos, eh mais verbosa mas facilita para obter a chave gerada 
+		 automaticamente
+		 
+		SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+		
+		List<String> columns = new ArrayList<>();
+		columns.add("name");
+		columns.add("duration");
+		
+		insert.setTableName("ride");
+		insert.setColumnNames(columns);
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("name", ride.getName());
+		data.put("duration", ride.getDuration());
+		
+		insert.setGeneratedKeyName("id");
+		
+		Number key = insert.executeAndReturnKey(data);
+		
+		System.out.println(key);
+		*/
+		
+		return null;
 	}
 	
 }
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -13,7 +13,7 @@ import org.junit.Test;
 
 public class RestControllerTest {
 
-	@Test(timeout=3000)
+	@Test(timeout=9000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
@@ -27,4 +27,20 @@ public class RestControllerTest {
 			System.out.println("Ride name: " + ride.getName());
 		}
 	}
+	
+	@Test(timeout=9000)
+	public void testCreateRide() {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Ride ride = new Ride();
+		ride.setName("Round Valley Ride");
+		ride.setDuration(38);
+		
+		ride = restTemplate.postForObject("http://localhost:8080/ride_tracker/ride", ride, Ride.class);
+		
+		System.out.println("Ride: " + ride);
+		
+	}
+	
 }
